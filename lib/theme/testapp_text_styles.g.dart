aba5894d09da8d6273ae4b1ce7a9b1431819a065
Parabeac-Nest <admin@parabeac.com>
2023-01-30 12:37:00 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/testapp_text_styles.g.dart
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TestappTextStyles {
+
+static const TextStyle m3LabelLarge = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.10000000149011612,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
